da85021ce212f253a4f4ea85f9f5254114485dd0
Joeri den Heijer <heijer.joeri@gmail.com>
2024-01-16 19:57:40 +0100

    update code: Joeri

--- Code_joeri/rushhour.py
+++ Code_joeri/rushhour.py
@@ -1,6 +1,12 @@
 from vehicle import Vehicle
 import csv
 import random
+import glob
+import os
+import time
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
 
 class RushHour(object):
     """A configuration of a single Rush Hour board."""
@@ -102,38 +108,106 @@ class RushHour(object):
 
 def load_file(rushhour_file):
     vehicles = []
-    dimension_board = 6
+    dim_board = extract_dimension(rushhour_file)
     with open(rushhour_file, 'r') as file:
             csv_reader = csv.reader(file)
             next(csv_reader)  # Skip header row
             for row in csv_reader:
                 id, orientation, x, y, length = row
                 vehicles.append(Vehicle(id, orientation, int(x) - 1, int(y) - 1, int(length)))
-            return RushHour(set(vehicles), dimension_board)
+            return RushHour(set(vehicles), dim_board)
 
 
 
-# def extract_dimension(filename):
-#     part = filename.split('/')[-1]
-#     dimension = part.split('x')[0]
-#     return int(dimension)
+def extract_dimension(filename):
+    gameboard_name = os.path.basename(filename).split('.')[0]
+    size_part = gameboard_name.split('_')[0]
+    dim_board = int(size_part.replace('Rushhour', '').split('x')[0])
+    return int(dim_board)
 
 
+def solve():
+    solutions_dict = {}
+    start_time = time.time()
+    for file in glob.glob('gameboards/*.csv'):
+        game_name = os.path.basename(file).split('.')[0]
+        rushhour_initial = load_file(file)
+        
+        number_of_steps = []
+
+        for _ in range(2): 
+            rushhour_current = rushhour_initial
+            
+            move_count = 0
+            while not rushhour_current.is_solved():
+                next_state = rushhour_current.make_random_move()
+                if next_state is None:
+                    print("No more moves available.")
+                    break
+                rushhour_current = next_state
+                move_count += 1
+                # print(rushhour_current) 
+            number_of_steps.append(move_count)
+            #print(f"Solved in {move_count} steps with random moves.")
+            
+        solutions_dict[game_name] = number_of_steps
+        print(solutions_dict[game_name])
+        
+    end_time = time.time()
+    total_time = end_time - start_time
+    print(f"Total execution time: {total_time} seconds")
+    return solutions_dict
+
+
+def plot(solutions_dict):
+    gameboards = []
+    steps = []
+    
+    for gameboard, step_counts in solutions_dict.items():
+        gameboards.extend([gameboard] * len(step_counts))
+        steps.extend(step_counts)
+
+    # Create a dataframe
+    data = pd.DataFrame({'Gameboard': gameboards, 'Steps': steps})
+
+    # Create a boxplot
+    plt.figure(figsize=(12, 8))
+
+    # Boxplot
+    sns.boxplot(data=data, x='Gameboard', y='Steps', palette="Set2")
+
+    # Adding scatterplot
+    sns.stripplot(data=data, x='Gameboard', y='Steps', color='black', alpha=0.5, jitter=True)
+
+    # Customizing plot
+    plt.xlabel('Gameboard')
+    plt.ylabel('Number of Steps')
+    plt.title('Boxplot and Scatterplot of Steps to Solve Rush Hour Puzzles per Gameboard')
+    plt.xticks(rotation=45)
+    plt.grid(True)
+    plt.tight_layout()
+
+    plt.show()
+
+
+    stats_data = []
+    for gameboard, steps in solutions_dict.items():
+        gameboard_data = pd.Series(steps)
+        stats = {
+            'Gameboard': gameboard,
+            'Mean': gameboard_data.mean(),
+            'Median': gameboard_data.median(),
+            'Min': gameboard_data.min(),
+            'Max': gameboard_data.max(),
+            'Std': gameboard_data.std()
+        }
+        stats_data.append(stats)
+
+    descriptive_stats_df = pd.DataFrame(stats_data)
+
+    print(descriptive_stats_df)
+    # descriptive_stats_df.to_csv('gameboard_statistics.csv', index=False)
 
 if __name__ == '__main__':
-    filename = "gameboards/Rushhour6x6_3.csv"
-    rushhour_initial = load_file(filename)
-    rushhour_current = rushhour_initial
-
-    move_count = 0
-    while not rushhour_current.is_solved():
-        next_state = rushhour_current.make_random_move()
-        if next_state is None:
-            print("No more moves available.")
-            break
-        rushhour_current = next_state
-        move_count += 1
-        print(rushhour_current) 
-
-    print(f"Solved in {move_count} steps with random moves.")
-
+    solution = solve()
+    plot(solution)
